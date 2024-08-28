1a344085597db00d4343bdc398e8eec2382a4a32
Remy Cantrelle <remycantrelle@Remys-MacBook-Pro-2.local>
2024-08-28 14:02:14 -0500

    d

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(220, 220);
+    background(150)
+    ellipse(60, 50, 15, 90)
+    ellipse(100, 50, 15, 90)
+    ellipse(80, 180, 25, 25)
+    ellipse(140, 180, 25, 25)
+    ellipse(150, 120, 30, 30)
+    ellipse(110, 150, 90, 70)
+    ellipse(80, 110, 50, 50)
+    stroke(0, 0, 0)
+    fill(0)
+    ellipse(70, 110, 10, 10)
+    stroke(0, 0, 0)
+    fill(200)
+    ellipse(71, 108, 5, 5)
+    stroke(0, 0, 0)
+    fill(0)
+    ellipse(90, 110, 10, 10)
+    stroke(0, 0, 0)
+    fill(200)
+    ellipse(91, 108, 5, 5)
+  }
